dab907eea2c6ecd6a8e1849f42c40298dee06bc1
MITHUL TAMILMANI <mithultamshri@gmail.com>
2026-03-30 08:12:55 +0530

    Updated Day6

---
+++ hopeetasks/Day8/encapsulationImplementation.java
@@ -0,0 +1,9 @@
+public class encapsulationImplementation {
+    Encapsulation(int a){
+        this.a = a;
+    }
+    void display(){
+        System.out.println("Implementation of Encapsulation"+b);
+        
+    }
+}
