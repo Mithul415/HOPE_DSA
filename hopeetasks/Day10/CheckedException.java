b0e3ea79be50b16ed1a1feb3bc64d288af918f3a
MITHUL TAMILMANI <mithultamshri@gmail.com>
2026-04-02 08:32:02 +0530

    Day10

---
+++ hopeetasks/Day10/CheckedException.java
@@ -0,0 +1,10 @@
+public class CheckedException {
+    public static void main(String[] args) {
+        try{
+            FileReader fileReader=new FileReader("abc.txt");
+            System.out.println(fileReader);
+        } catch (Exception e) {
+            System.out.println("An error occurred: " + e.getMessage());
+        }
+    }
+}
